ad7599ac90f8eb1aa3f1a8dca3e51dafb4edfa1a
saynotrish <anuran.nyleco24@gmail.com>
2023-07-25 15:38:12 +0800

    Header Animation web and media

--- dist/JS/script.js
+++ dist/JS/script.js
@@ -4,3 +4,21 @@ toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerNav.classList.toggle("open");
 });
+
+const drop = document.querySelectorAll(".header__nav ul.nav li");
+drop.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.nextElementSibling.classList.toggle("open");
+    link.querySelector("i").classList.toggle("open");
+  });
+});
+
+const kamote = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY >= 500) {
+    kamote.classList.add("white");
+  } else {
+    kamote.classList.remove("white");
+  }
+});
